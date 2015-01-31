14324493e2ea210a17d5644ca740ea48db495ae5
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-31 15:41:37 -0800

    Fixed unicorn-blinky, it blinks awesomely :D

--- cmd/unicorn-blinky/unicorn-blinky.go
+++ cmd/unicorn-blinky/unicorn-blinky.go
@@ -1,6 +1,8 @@
 package main
 
 import "github.com/DrItanium/unicornhat"
+import "time"
+import "fmt"
 
 func terminate_unicorn(status int) {
 	for i := 0; i < 64; i++ {
@@ -11,15 +13,22 @@ func terminate_unicorn(status int) {
 }
 func main() {
 	unicornhat.SetBrightness(unicornhat.DefaultBrightness())
-	unicornhat.InitHardware()
+	unicornhat.Init(64)
 	unicornhat.ClearLEDBuffer()
 	for count := 0; count < 64; count++ {
+		fmt.Println("count = ", count)
 		unicornhat.SetPixelColor(0,1,1,1)
+		unicornhat.Show()
+		time.Sleep(10 * time.Microsecond)
 		for index := 1; index < 64; index++ {
 			unicornhat.SetPixelColor(uint(index - 1), 0, 0, 0)
-			unicornhat.SetPixelColor(uint(index), 1, 1, 1)
+			unicornhat.SetPixelColor(uint(index), 255, 255, 255)
+			unicornhat.Show()
+			time.Sleep(10)
 		}
 		unicornhat.SetPixelColor(63,0,0,0)
+		unicornhat.Show()
+		time.Sleep(10 * time.Microsecond)
 	}
 	defer terminate_unicorn(0)
 }
