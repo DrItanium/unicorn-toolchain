ee425dafc8e8357788a041241b39332b8927ab2d
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-31 16:51:49 -0800

    unicorn-blinky now runs forever until ctrl-c is pressed

--- cmd/unicorn-blinky/unicorn-blinky.go
+++ cmd/unicorn-blinky/unicorn-blinky.go
@@ -2,31 +2,44 @@ package main
 
 import "github.com/DrItanium/unicornhat"
 import "time"
+import "os"
+import "os/signal"
+import "syscall"
 
 func terminate_unicorn(status int) {
 	for i := 0; i < 64; i++ {
-		unicornhat.SetPixelColor(uint(i),0,0,0)
+		unicornhat.SetPixelColor(uint(i), 0, 0, 0)
 	}
 	unicornhat.Show()
 	unicornhat.Shutdown(status)
 }
+func microsecond_delay(usec time.Duration) {
+	time.Sleep(usec * time.Microsecond)
+}
 func main() {
+	running := true
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT)
+	go func() {
+		<-signalChan
+		running = false
+	}()
 	unicornhat.SetBrightness(unicornhat.DefaultBrightness())
 	unicornhat.Init(64)
 	unicornhat.ClearLEDBuffer()
-	for count := 0; count < 64; count++ {
-		unicornhat.SetPixelColor(0,1,1,1)
+	for running {
+		unicornhat.SetPixelColor(0, 1, 1, 1)
 		unicornhat.Show()
 		time.Sleep(10 * time.Microsecond)
 		for index := 1; index < 64; index++ {
-			unicornhat.SetPixelColor(uint(index - 1), 0, 0, 0)
+			unicornhat.SetPixelColor(uint(index-1), 0, 0, 0)
 			unicornhat.SetPixelColor(uint(index), 255, 255, 255)
 			unicornhat.Show()
-			time.Sleep(10)
+			microsecond_delay(10)
 		}
-		unicornhat.SetPixelColor(63,0,0,0)
+		unicornhat.SetPixelColor(63, 0, 0, 0)
 		unicornhat.Show()
-		time.Sleep(10 * time.Microsecond)
+		microsecond_delay(10)
 	}
 	defer terminate_unicorn(0)
 }
