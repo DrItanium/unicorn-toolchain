1d3ad0bdcb61e42002780799d1cafc5a7cdae307
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-31 16:20:48 -0800

    Unicorn smear now runs until control c is pressed

--- cmd/unicorn-smear/unicorn-smear.go
+++ cmd/unicorn-smear/unicorn-smear.go
@@ -2,6 +2,10 @@ package main
 
 import "github.com/DrItanium/unicornhat"
 import "time"
+import "math/rand"
+import "os"
+import "os/signal"
+import "syscall"
 
 func terminate_unicorn(status int) {
 	for i := 0; i < 64; i++ {
@@ -13,44 +17,41 @@ func terminate_unicorn(status int) {
 func microsecond_delay(usec time.Duration) {
 	time.Sleep(usec * time.Microsecond)
 }
+func random_byte() byte {
+	return byte(rand.Int())
+}
 func main() {
+	running := true
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT)
+	go func() {
+		<-signalChan
+		running = false
+	}()
 	unicornhat.SetBrightness(unicornhat.DefaultBrightness())
 	unicornhat.Init(64)
 	unicornhat.ClearLEDBuffer()
-	for count := 0; count < 64; count++ {
-		unicornhat.SetPixelColor(0, 255, 255, 255)
+	for running {
+		unicornhat.SetPixelColor(0, random_byte(), random_byte(), random_byte())
 		unicornhat.Show()
 		microsecond_delay(10)
 		for index := 1; index < 64; index++ {
-			unicornhat.SetPixelColor(uint(index), 255, 255, 255)
-			for prev := 0; prev < index; prev++ {
-				pix := unicornhat.GetPixelColor(uint(prev))
-				unicornhat.SetPixelColor(uint(prev), pix.R-1, pix.G-1, pix.B-1)
-			}
-			unicornhat.Show()
-			microsecond_delay(10)
-		}
-		shouldContinue := true
-		for shouldContinue {
-			shouldContinue = false
-			for i := 0; i < 64; i++ {
+			unicornhat.SetPixelColor(uint(index), random_byte(), random_byte(), random_byte())
+			for i := 0; i < index; i++ {
 				pix := unicornhat.GetPixelColor(uint(i))
 				var r, g, b byte
 				if pix.R > 0 {
 					r = pix.R - 1
-					shouldContinue = true
 				} else {
 					r = 0
 				}
 				if pix.G > 0 {
 					g = pix.G - 1
-					shouldContinue = true
 				} else {
 					g = 0
 				}
 				if pix.B > 0 {
 					b = pix.B - 1
-					shouldContinue = true
 				} else {
 					b = 0
 				}
@@ -61,5 +62,5 @@ func main() {
 		}
 		microsecond_delay(10)
 	}
-	defer terminate_unicorn(0)
+	terminate_unicorn(0)
 }
