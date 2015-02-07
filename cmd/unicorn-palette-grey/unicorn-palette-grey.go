7ea03a93c6b3875f4cc7cf6aff8aadc807a18987
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-02-07 11:49:36 -0800

    Added options for randomize and horizontal output

--- cmd/unicorn-palette-grey/unicorn-palette-grey.go
+++ cmd/unicorn-palette-grey/unicorn-palette-grey.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"github.com/DrItanium/unicornhat"
+	"math/rand"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
+
+var coords = flag.Bool("horizontal", false, "display lights in a horzontal fashion instead of vertical")
+var randomize = flag.Bool("randomize", false, "randomize the position to select")
 
 func terminate_unicorn(status int) {
 	for i := 0; i < 64; i++ {
@@ -19,8 +24,26 @@ func terminate_unicorn(status int) {
 func microsecond_delay(usec time.Duration) {
 	time.Sleep(usec * time.Microsecond)
 }
+func showPixel(input *bufio.Reader, i int) bool {
+	var pixel unicornhat.Pixel
+	tmp, err := input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.R = tmp
+		pixel.G = tmp
+		pixel.B = tmp
+	}
+	unicornhat.SetPixelColorType(uint(i), pixel)
+	unicornhat.Show()
+	microsecond_delay(100)
+	return true
+}
+
 func main() {
 	defer terminate_unicorn(0)
+	flag.Parse()
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	running := true
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT)
@@ -34,21 +57,35 @@ func main() {
 	input := bufio.NewReader(os.Stdin)
 	// setup the initial pixels
 	for running {
-		for i := 0; i < 64; i++ {
-			var pixel unicornhat.Pixel
-			tmp, err := input.ReadByte()
-			if err != nil {
-				running = false
-			} else {
-				pixel.R = tmp
-				pixel.G = tmp
-				pixel.B = tmp
+		if *coords {
+			for y := 0; y < 8; y++ {
+				for x := 0; x < 8; x++ {
+					vY := y
+					vX := x
+					if *randomize && (r.Int()%2 == 1) {
+						vY = r.Int() % 8
+						vX = r.Int() % 8
+					}
+					running = showPixel(input, unicornhat.CoordinateToPosition(vX, vY)) && running
+					if !running {
+						break
+					}
+				}
+				if !running {
+					break
+				}
 			}
-			unicornhat.SetPixelColorType(uint(i), pixel)
-			unicornhat.Show()
-			microsecond_delay(10)
-			if !running {
-				break
+		} else {
+			for i := 0; i < 64; i++ {
+				vI := i
+				if *randomize && (r.Int()%2 == 1) {
+					vI = r.Int() % 64
+				}
+				running = showPixel(input, vI) && running
+				if !running {
+					break
+				}
+
 			}
 		}
 	}
