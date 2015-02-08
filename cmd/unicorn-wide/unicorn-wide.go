30f47df8579243672cd801e5c421600f3bc93d7f
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-02-08 11:28:05 -0800

    Added a delay check in the unicorn-wide by consuming an extra byte and added full page mode

--- cmd/unicorn-wide/unicorn-wide.go
+++ cmd/unicorn-wide/unicorn-wide.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"github.com/DrItanium/unicornhat"
 	"math/rand"
 	"os"
@@ -9,6 +10,11 @@ import (
 	"syscall"
 	"time"
 )
+
+var fullpageUpdate = flag.Bool("fullpage", false, "Perform full page updates instead of pixel by pixel")
+
+//var rowbyrowUpdate = flag.Bool("rowbyrow", false, "Peform
+var running = true
 
 func terminate_unicorn(status int) {
 	for i := 0; i < 64; i++ {
@@ -25,17 +31,53 @@ func random_byte() byte {
 }
 
 func readPixelByPixelQuantum(input *bufio.Reader) (*unicornhat.Pixel, error) {
-	elements := make([]byte, 3)
+	elements := make([]byte, 4)
 	count, err := input.Read(elements)
 	if err != nil && count == 0 {
 		return nil, err
 	}
-	return &unicornhat.Pixel{R: elements[0], G: elements[1], B: elements[2]}, err
+	microsecond_delay(time.Duration(elements[3]))
+	return unicornhat.NewPixel(elements[0], elements[1], elements[2]), err
+}
+
+func pixelByPixelUpdate(input *bufio.Reader) {
+	for i := 0; i < 64; i++ {
+		if !running {
+			return
+		}
+		pixel, err := readPixelByPixelQuantum(input)
+		if err != nil {
+			running = false
+		}
+		if pixel != nil {
+			unicornhat.SetPixelColorType(uint(i), pixel)
+		}
+		unicornhat.Show()
+	}
+}
+
+func fullPageUpdate(input *bufio.Reader) {
+	elements := make([]byte, 256)
+	count, err := input.Read(elements)
+	if err != nil && count == 0 {
+		running = false
+		return
+	}
+	var pixel unicornhat.Pixel
+	for i := 0; i < 256; i += 4 {
+		pixel.R = elements[i]
+		pixel.G = elements[i+1]
+		pixel.B = elements[i+2]
+		delay := elements[i+3]
+		unicornhat.SetPixelColorType(uint(i/4), &pixel)
+		microsecond_delay(time.Duration(delay))
+	}
+	unicornhat.Show()
 }
 
 func main() {
 	defer terminate_unicorn(0)
-	running := true
+	flag.Parse()
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT)
 	go func() {
@@ -46,17 +88,10 @@ func main() {
 	unicornhat.ClearLEDBuffer()
 	input := bufio.NewReader(os.Stdin)
 	for running {
-		for i := 0; i < 64; i++ {
-			if !running {
-				break
-			}
-			pixel, err := readPixelByPixelQuantum(input)
-			if err != nil {
-				running = false
-			}
-			if pixel != nil {
-				unicornhat.SetPixelColorType(uint(i), pixel)
-			}
+		if *fullpageUpdate {
+			fullPageUpdate(input)
+		} else {
+			pixelByPixelUpdate(input)
 		}
 	}
 }
