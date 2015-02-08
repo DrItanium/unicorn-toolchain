219f0b6f9e436e04afeb127ccede22564fef66d5
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-02-07 22:33:57 -0800

    Added presets for different color palettes

--- cmd/unicorn-palette-grey/unicorn-palette-grey.go
+++ cmd/unicorn-palette-grey/unicorn-palette-grey.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"github.com/DrItanium/unicornhat"
 	"math/rand"
 	"os"
@@ -14,6 +15,18 @@ import (
 var coords = flag.Bool("horizontal", false, "display lights in a horzontal fashion instead of vertical")
 var randomize = flag.Bool("randomize", false, "randomize the position to select")
 
+var preset = flag.Int("preset", 0, "select a predefined color palette index.\n\t0 - all colors\n\t1 - greyscale\n\t2 - green\n\t3 - yellow\n\t4 - yellow and green\n\t5 - purple\n\t6 - cyan")
+
+const (
+	FullColorPreset = iota
+	Greyscale
+	Green
+	Yellow
+	GreenAndYellow
+	Purple
+	Cyan
+)
+
 func terminate_unicorn(status int) {
 	for i := 0; i < 64; i++ {
 		unicornhat.SetPixelColor(uint(i), 0, 0, 0)
@@ -24,7 +37,7 @@ func terminate_unicorn(status int) {
 func microsecond_delay(usec time.Duration) {
 	time.Sleep(usec * time.Microsecond)
 }
-func showPixel(input *bufio.Reader, i int) bool {
+func greyscalePixel(input *bufio.Reader, i int) bool {
 	var pixel unicornhat.Pixel
 	tmp, err := input.ReadByte()
 	if err != nil {
@@ -36,8 +49,120 @@ func showPixel(input *bufio.Reader, i int) bool {
 	}
 	unicornhat.SetPixelColorType(uint(i), pixel)
 	unicornhat.Show()
-	microsecond_delay(100)
+	microsecond_delay(10)
 	return true
+}
+func colorPixel(input *bufio.Reader, i int) bool {
+	var pixel unicornhat.Pixel
+	tmp, err := input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.R = tmp
+	}
+	tmp, err = input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.G = tmp
+	}
+	tmp, err = input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.B = tmp
+	}
+	unicornhat.SetPixelColorType(uint(i), pixel)
+	unicornhat.Show()
+	microsecond_delay(10)
+	return true
+}
+func purplePixel(input *bufio.Reader, i int) bool {
+	// purple is made up of red and blue so no green
+	var pixel unicornhat.Pixel
+	pixel.G = 0
+	tmp, err := input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.R = tmp
+		pixel.B = tmp
+	}
+	unicornhat.SetPixelColorType(uint(i), pixel)
+	unicornhat.Show()
+	microsecond_delay(10)
+	return true
+}
+func cyanPixel(input *bufio.Reader, i int) bool {
+	// cyan is made up of green and blue so no red
+	var pixel unicornhat.Pixel
+	pixel.R = 0
+	tmp, err := input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.G = tmp
+		pixel.B = tmp
+	}
+	unicornhat.SetPixelColorType(uint(i), pixel)
+	unicornhat.Show()
+	microsecond_delay(10)
+	return true
+}
+func yellowPixel(input *bufio.Reader, i int) bool {
+	// yellow is made up of red and green so no blue
+	var pixel unicornhat.Pixel
+	pixel.B = 0
+	tmp, err := input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.R = tmp
+		pixel.G = tmp
+	}
+	unicornhat.SetPixelColorType(uint(i), pixel)
+	unicornhat.Show()
+	microsecond_delay(10)
+	return true
+}
+func greenPixel(input *bufio.Reader, i int) bool {
+	var pixel unicornhat.Pixel
+	tmp, err := input.ReadByte()
+	if err != nil {
+		return false
+	} else {
+		pixel.G = tmp
+	}
+	unicornhat.SetPixelColorType(uint(i), pixel)
+	unicornhat.Show()
+	microsecond_delay(10)
+	return true
+
+}
+func showPixel(input *bufio.Reader, i int) bool {
+	switch *preset {
+	case FullColorPreset:
+		return colorPixel(input, i)
+	case Greyscale:
+		return greyscalePixel(input, i)
+	case Green:
+		return greenPixel(input, i)
+	case Yellow:
+		return yellowPixel(input, i)
+	case GreenAndYellow:
+		if rand.Int()%2 == 1 {
+			return greenPixel(input, i)
+		} else {
+			return yellowPixel(input, i)
+		}
+	case Purple:
+		return purplePixel(input, i)
+	case Cyan:
+		return cyanPixel(input, i)
+	default:
+		fmt.Println("Invalid preset ", *preset, "provided!")
+		return false
+	}
 }
 
 func main() {
